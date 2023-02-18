180fcdfa8d8556a897243477f942e75fe2cf6bc4
eneseray <enes.eraay@gmail.com>
2023-02-18 21:19:02 +0300

    AnimeGAN2FacePaint added

---
+++ AnimeGAN2FacePaint/AnimeGAN2FacePaint/AnimeGAN2FacePaintApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeGAN2FacePaintApp.swift
+//  AnimeGAN2FacePaint
+//
+//  Created by Enes Eray on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimeGAN2FacePaintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
