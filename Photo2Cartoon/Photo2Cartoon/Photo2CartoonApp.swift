bbf05ae4ee39fc6d8401579c8786bf897f33762f
eneseray <enes.eraay@gmail.com>
2023-02-18 21:19:45 +0300

    Photo2Cartoon added.

---
+++ Photo2Cartoon/Photo2Cartoon/Photo2CartoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo2CartoonApp.swift
+//  Photo2Cartoon
+//
+//  Created by Enes Eray on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Photo2CartoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
