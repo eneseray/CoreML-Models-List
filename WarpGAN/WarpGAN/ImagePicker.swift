499792b0d0f6ca041e9a3129d9b670369c7d2bef
eneseray <enes.eraay@gmail.com>
2023-02-18 21:41:55 +0300

    WarpGAN added.

---
+++ WarpGAN/WarpGAN/ImagePicker.swift
@@ -0,0 +1,41 @@
+//
+//  ImagePicker.swift
+//  WarpGAN
+//
+//  Created by Enes Eray on 18.02.2023.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var image:UIImage
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.allowsEditing = true
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(imgPicker: self)
+    }
+    
+    final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+        let imgPicker:ImagePicker
+        init(imgPicker:ImagePicker){
+            self.imgPicker = imgPicker
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.editedImage] as? UIImage {
+                imgPicker.image = image
+            } else {
+                // return error
+            }
+            picker.dismiss(animated: true)
+        }
+    }
+}
---
+++ WarpGAN/WarpGAN/WarpGANApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarpGANApp.swift
+//  WarpGAN
+//
+//  Created by Enes Eray on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WarpGANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
